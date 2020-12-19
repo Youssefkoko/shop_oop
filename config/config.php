72a70aa1e03f2a13b9220069e942b547e39532f7
Sonja <youssefkoko01@gmail.com>
2020-12-19 15:40:05 +0100

    initial commit

--- config/config.php
+++ config/config.php
@@ -1,6 +1,7 @@
 <?php
 
+
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'shop_oop');
 define('DB_USER', 'youssef-x');
-define('DB_PASS', '123456');
+define('DB_PASS', 'koko/8520*K?');
--- lib/Database.php
+++ lib/Database.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once '../config/config.php';
 /**
  * DB PDO CLASS 
  * Connect to db 
