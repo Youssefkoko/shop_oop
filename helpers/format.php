2384abacc0dbdc80e5bc7218c42e0c2664863a6d
Sonja <youssefkoko01@gmail.com>
2020-12-19 15:35:29 +0100

    start session class

--- helpers/format.php
+++ helpers/format.php
@@ -4,5 +4,10 @@
  * Format Class
  */
 class Format {
-   
+   public function validation($data){
+     $data = trim($data);
+     $data = stripcslashes($data);
+     $data = htmlspecialchars($data);
+     return $data;
+   }
 }
